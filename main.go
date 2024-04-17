b49e7a18c540ca7f46bef41a13286d9c5446c048
Nerijus Rozga <nerijusrozga@Nerijuss-MBP.lan>
2024-04-17 20:06:36 +0300

    Add GET and POST for scooters

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"ScootinAboot/api"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	api.AddScooterHandlerEndpoints(r)
+
+	r.Run(":8000")
 }
--- types/scooter.go
+++ types/scooter.go
@@ -1,8 +1,17 @@
 package types
 
+import "github.com/google/uuid"
+
+type ScooterStatus string
+
+const (
+	Available = "available"
+	Taken     = "taken"
+)
+
 type Scooter struct {
-	ID         string   `json:"id"`
-	Location   Location `json:"location"`
-	Status     string   `json:"status"`
-	OccupiedBy string   `json:"occupied_by"`
+	ID         uuid.UUID     `json:"id"`
+	Location   Location      `json:"location"`
+	Status     ScooterStatus `json:"status"`
+	OccupiedBy uuid.UUID     `json:"occupied_by"`
 }
